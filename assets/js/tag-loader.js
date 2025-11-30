d5e98ed04ee1d63f8c737cfcfc009a0aa653c6eb
tripp113 <165693357+tripp113@users.noreply.github.com>
2025-11-30 14:39:23 -0500

    Initial site files

---
+++ assets/js/tag-loader.js
@@ -0,0 +1,6 @@
+
+// Placeholder tag logic
+document.addEventListener("DOMContentLoaded", () => {
+    const content = document.getElementById("content");
+    content.innerHTML = `<h2>Welcome to Sheer Carnage</h2><p>This is the firestarter build.</p>`;
+});
